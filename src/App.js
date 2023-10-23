6fec3d17b23c0e2e3d12eb0d413c162cf1a623ee
imran-py <imran.dtc144@gmail.com>
2023-10-23 17:47:51 +0530

    percentage in margin better choice instead px

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import Margin_px_percentage from './Margin_px_percentage'
 // import CssProperties from './CssProperties'
 // import MarginPadding from './MarginPadding'
-import Diff_MarginPadding from './Diff_MarginPadding'
+// import Diff_MarginPadding from './Diff_MarginPadding'
 
 const App = () => {
   return (
     <React.Fragment>
       {/* <CssProperties /> */}
       {/* <MarginPadding /> */}
-      <Diff_MarginPadding />
+      {/* <Diff_MarginPadding /> */}
+      <Margin_px_percentage />
     </React.Fragment>
   )
 }
---
+++ src/Margin_px_percentage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './mycss/margin_px_per.css'
+const Margin_px_percentage = () => {
+  return (
+    <div>
+      <p>this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph 
+      this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph 
+      this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph 
+      this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph this is paragraph 
+      </p>
+
+    </div>
+  )
+}
+
+export default Margin_px_percentage
