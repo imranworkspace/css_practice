9903dd695de9ab4969a32848e405a4458d907565
imran-py <imran.dtc144@gmail.com>
2023-10-24 12:01:46 +0530

    border-left properties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import BorderLeft from './BorderLeft'
 // import Margin_px_percentage from './Margin_px_percentage'
-import Opacity_properties from './Opacity_properties'
+// import Opacity_properties from './Opacity_properties'
 // import CssProperties from './CssProperties'
 // import MarginPadding from './MarginPadding'
 // import Diff_MarginPadding from './Diff_MarginPadding'
@@ -13,7 +14,8 @@ const App = () => {
       {/* <Diff_MarginPadding /> */}
       {/* <Margin_px_percentage /> */}
 
-      <Opacity_properties />
+      {/* <Opacity_properties /> */}
+      <BorderLeft />
     </React.Fragment>
   )
 }
