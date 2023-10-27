531eb61881abc7a7e2107a9c18b3923c943afd26
imran-py <imran.dtc144@gmail.com>
2023-10-27 17:18:00 +0530

    flex_justify start prop

---
+++ src/FlexJustify.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './mycss/flex_justify.css'
+const Flex = () => {
+  return (
+    <div>
+      <div id='container'>
+        <div id='firstdiv' className='div'>1</div>
+        <div id='seconddiv' className='div'>2</div>
+        <div id='thirddiv' className='div'>3</div>
+        <div id='forthdiv' className='div'>4</div>
+        <div id='fifthdiv' className='div'>5</div>
+      </div>
+    </div>
+  )
+}
+export default Flex
